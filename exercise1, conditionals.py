f2c4fc1408dfe292c21de88ceb4a461e0ed3178d
ArishMada <arishmadataly@gmail.com>
2021-10-05 14:10:04 +0300

    removed a space

--- exercise1, conditionals.py
+++ exercise1, conditionals.py
@@ -1,4 +1,4 @@
-Select_the_operation = input('Choose a number (1.Addition, 2.Subtraction, 3.Multiplication, 4.Division): ')
+Select_the_operation = input('Choose a number(1.Addition, 2.Subtraction, 3.Multiplication, 4.Division): ')
 n1 = int(input('Enter a first number: '))
 n2 = int(input('Enter a second number: '))
 
