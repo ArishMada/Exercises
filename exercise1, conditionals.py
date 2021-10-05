b4af3c56f8976efacc5c052f1eed5e5d0d9269c8
ArishMada <arishmadataly@gmail.com>
2021-10-05 13:39:52 +0300

    Create exercise1, conditionals.py
    First exercice in tutoring class

---
+++ exercise1, conditionals.py
@@ -0,0 +1,17 @@
+Select_the_operation = input('Choose a number (1.Addition, 2.Subtraction, 3.Multiplication, 4.Division): ')
+n1 = int(input('Enter a first number: '))
+n2 = int(input('Enter a second number: '))
+
+if Select_the_operation == '1':
+    print(f'The result is {n1 + n2}')
+elif Select_the_operation == '2':
+    print(f'The result is {n1 - n2}')
+elif Select_the_operation == '3':
+    print(f'The result is {n1 * n2}')
+elif Select_the_operation == '4':
+    print(f'The result is {n1 / n2}')
+
+else:
+    print('error')
+
+
